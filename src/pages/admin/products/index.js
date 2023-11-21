46656fff4ee9c64b2851e288bc76ce8cbca6c1cc
chrod <chrod.dev@gmail.com>
2023-11-21 15:57:09 -0500

    Add content to products page

--- src/pages/admin/products/index.js
+++ src/pages/admin/products/index.js
@@ -1,3 +1,15 @@
-const Products = () => {};
+import DataTable from '@components/data/DataTable';
+import { useProductsContext } from '@contexts/products/ProductsContext';
+import { Grid } from '@mui/material';
+
+const Products = () => {
+	const { products } = useProductsContext();
+
+	return (
+		<Grid item xs={12}>
+			<DataTable products={products} />
+		</Grid>
+	);
+};
 
 export default Products;
